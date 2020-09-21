77b55c32408c4993df3d03b3e4ded09883f30c61
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-20 21:41:04 -0500

    add Post scaffold with all features and edit views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+	resources :posts, only: [:index, :new, :destroy ]
+ 
   devise_for :users, :controllers => { registrations: 'registrations' }
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
