91ca81796ece4dea595b8ffc4a44006430663985
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-22 10:34:16 -0500

    add style to views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+
+
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+      @posts = Post.all
   end
 
   # GET /posts/1
