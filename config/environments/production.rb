c6c3dafc9a46f8dee5b26209ef1cf38f2a6c804d
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-25 11:19:20 -0500

    change app key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
+  config.secret_key_base = 'blipblapblup'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
