f5c1b94ce088bb5046bf598505f63381630108a1
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-25 09:55:14 -0500

    fix linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,16 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(post_id: params[:post_id])
 
-    redirect_to posts_path if @like.save
-    redirect_to posts_path
+    if @like.save
+      redirect_to posts_path, notice: 'Like'
+    else
+      redirect_to posts_path
+    end
   end
 
   def destroy
     like = Like.find_by(id: params[:id], user: current_user, post_id: params[:post_id])
-    if like
-      like.destroy
-      redirect_to posts_path
-    end
-    redirect_to posts_path
+    like&.destroy
+    redirect_to root_path
   end
 end
