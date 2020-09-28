3920927d4f53b0a2d6fcc304568d57670a65147a
Jose Pedraza <pastorpedraza3@gmail.com>
2020-09-28 09:22:34 -0500

    add test for validations in models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -17,4 +17,9 @@ RSpec.describe Comment, type: :model do
     test_c = Comment.create(post_id: test_p.id, user_id: test_u.id, content: 'this is a test comment')
     expect(test_c).to be_valid
   end
+
+  it '3. FAIL CHECK - Checks if a user can be created without content' do
+    test_c = Comment.create(post_id: nil, user_id: nil, content: nil)
+    expect(test_c).to be_invalid
+  end
 end
